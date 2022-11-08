f65a55e347c969da2fb85e0795e2debf5a7f4fa7
Guilherme Vanderley <guivdouradog@gmail.com>
2022-11-08 17:58:27 -0300

    receiving params

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { useRoute, RouteProp } from '@react-navigation/native';
+
+type RouteDetailParams = {
+  Order: {
+    table: string | number;
+    order_id: string;
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
 
 export default function Order(){
+  const route = useRoute<OrderRouteProps>();
+  
   return(
     <View style={styles.container}>
       <Text style={styles.title}>Order Screen</Text>
+      <Text>{route.params.table}</Text>
     </View>
   )
 }
