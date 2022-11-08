9d97743228144bfa248beeb7ef246255636783d4
Guilherme Vanderley <guivdouradog@gmail.com>
2022-11-07 21:52:29 -0300

    add signout to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,16 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { useContext } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Dashboard(){
+  const { signOut } = useContext(AuthContext);
+  
   return(
     <View>
       <Text>Dashboard stuff</Text>
+      <TouchableOpacity style={{ width: '100%', backgroundColor: 'blue', height: 40, alignItems: 'center', justifyContent: 'center'}} onPress={signOut}>
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>Sair</Text>
+      </TouchableOpacity>
     </View>
   )
 }
